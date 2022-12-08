dc1edf051c9eb3f632819ea01b5c1ef451c19d34
Burstniart <v4gr33n@hotmail.com>
2022-12-08 00:41:24 -0600

    Build is taking too long, will try tomorrow

--- README.md
+++ README.md
@@ -6,7 +6,13 @@ https://imgur.com/gallery/5cbaf
 
 notes to self:
 get the project live (if render deployment fails the try with express in fly)
+Deployment sucessfull on:
+
+https://pink-apple.fly.dev/
+
+But now need to fi MIME types to recognize js and CSS
 get the date once clicked on (a console.log will do for now)
+
 set up an appointment function to send an event name, the date and time
 set up a data base
 save messages from appointment function to database
--- app.js
+++ app.js
@@ -1,26 +1,16 @@
 const express = require('express')
-const app = express()
-const port = 3000
-const path = require('path')
-const http = require('http')
-const finalhandler = require('finalhandler')
-const serveStatic = require('serve-static')
 
-/*
+const app = express()
+const port = process.env.PORT 
+const path = require('path')
+
+
 app.get('/',(req, res) => {
     res.sendFile(path.join(__dirname, 'src/index.html'))
 })
-*/
 
-var serve = serveStatic('public/ftp', { index: 'src/index.html'})
 
-var server = http.createServer(function onRequest (req, res) {
-    serve(req, res, finalhandler(req,res))
-})
-server.listen(port)
-
-/*
 app.listen(port, () => {
     console.log(`The pink apple is at the ${port} tree`)
 })
-*/
+
