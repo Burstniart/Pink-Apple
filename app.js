539dc63b6258e6257748800669af07cab82d1a7e
Burstniart <v4gr33n@hotmail.com>
2022-12-09 23:50:22 -0600

    Deployment succesful

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 const port = process.env.PORT 
 const path = require('path')
 
+// Serve Js and CSS files
+app.use( express.static(path.join(__dirname, 'src')));
 
 app.get('/',(req, res) => {
     res.sendFile(path.join(__dirname, 'src/index.html'))
