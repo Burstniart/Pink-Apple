0ae04aab5237ff1a48c8eab2e33aca21445dca26
Burstniart <v4gr33n@hotmail.com>
2022-12-20 00:57:18 -0600

    Pending mongo in docker to configure database communication

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
 const express = require('express')
 
 const app = express()
+app.use(express.json()) // Indicate we're using JSON
 const port = process.env.PORT || 3000
 const path = require('path')
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+//mongodb lines
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+mongoose.connect("");
+
+let dateSchema = new mongoose.Schema({
+    date: String,
+    msg: String,
+})
+
+let dateNight = mongoose.model("dateNight", dateSchema);
+
 
 // Serve Js and CSS files
 app.use( express.static(path.join(__dirname, 'src')));
@@ -12,11 +29,21 @@ app.get('/',(req, res) => {
 })
 
 // test new route / response
-app.get('/test', (req,res) => {
+app.post('/addDate', (req,res) => {
+    const myDate = new dateNight(req.body)
+    myDate.save()
+        .then(item => {
+            res.send("item saved to database")
+        })
+        .catch(err => {
+            res.status(400).send("unable to sabe to database")
+        })
+
     const dateNight = {date: "2022-12-18", msg:"Monika date night"}
-    res.send(dateNight)
+    res.send(req.params)
 })
 
+// http://localhost:3000/test?inputDates=2022-12-03&msg=sksk
 
 // console.log every date with name as json 
 app.listen(port, () => {
